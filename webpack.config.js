4f8b47b1e994df573340850b255f393e7758627f
zakuni <kunio038@gmail.com>
2017-02-19 00:35:15 +0900

    turn off babel to convert import and export
    since webpack2 can now handle it

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
+        loader: 'babel-loader'
       }
     ]
   }
