fc11cb219699784df2392e2ce4640b5f845938c3
zakuni <kunio038@gmail.com>
2017-02-19 01:04:01 +0900

    don't need to rely on $.ready

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const ScrambleTitle = require('./font-family.js.cjsx');
 
-$(() => {
-  ReactDOM.render(
-    <ScrambleTitle />,
-    document.getElementById('title')
-  );
-});
+ReactDOM.render(
+  <ScrambleTitle />,
+  document.getElementById('title')
+);
+
