ab4d0e38a7b8b7468c905ef8d787d5dd7de6f88b
zakuni <kunio038@gmail.com>
2017-02-19 23:36:41 +0900

    modify ScrambleTitle

--- source/javascripts/components/ScrambleTitle.js
+++ source/javascripts/components/ScrambleTitle.js
@@ -2,34 +2,33 @@ import React from 'react';
 import Scramble from './Scramble.cjsx';
 import { Letter, Space, Period } from './font-family';
 
-const ScrambleTitle = (props) => {
-  const ScrambleO = Scramble(Letter, "o");
-  const ScrambleZ = Scramble(Letter, "z");
-  const ScrambleA = Scramble(Letter, "a");
-  const ScrambleW = Scramble(Letter, "w");
-  const ScrambleK = Scramble(Letter, "k");
-  const ScrambleU = Scramble(Letter, "u");
-  const ScrambleN = Scramble(Letter, "n");
-  const ScrambleI = Scramble(Letter, "i");
-  const ScrambleSpace = Scramble(Space);
-  const ScramblePeriod = Scramble(Period);
-  return (
-    <h1 className="scrambleTitle uk-container-center uk-heading-large">
-      <ScrambleO />
-      <ScrambleZ />
-      <ScrambleA />
-      <ScrambleW />
-      <ScrambleA />
-      <ScrambleSpace />
-      <ScrambleK />
-      <ScrambleU />
-      <ScrambleN />
-      <ScramblePeriod />
-      <ScrambleI />
-      <ScrambleO />
-    </h1>
-  );
-}
+const ScrambleO = Scramble(Letter, "o");
+const ScrambleZ = Scramble(Letter, "z");
+const ScrambleA = Scramble(Letter, "a");
+const ScrambleW = Scramble(Letter, "w");
+const ScrambleK = Scramble(Letter, "k");
+const ScrambleU = Scramble(Letter, "u");
+const ScrambleN = Scramble(Letter, "n");
+const ScrambleI = Scramble(Letter, "i");
+const ScrambleSpace = Scramble(Space);
+const ScramblePeriod = Scramble(Period);
+
+const ScrambleTitle = (props) => (
+  <h1 className="scrambleTitle uk-container-center uk-heading-large">
+    <ScrambleO />
+    <ScrambleZ />
+    <ScrambleA />
+    <ScrambleW />
+    <ScrambleA />
+    <ScrambleSpace />
+    <ScrambleK />
+    <ScrambleU />
+    <ScrambleN />
+    <ScramblePeriod />
+    <ScrambleI />
+    <ScrambleO />
+  </h1>
+);
 
 export default ScrambleTitle
 
