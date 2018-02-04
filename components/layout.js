4b55c866f4147194cb4bf810e24da1e4c9d085f6
zakuni <kunio038@gmail.com>
2018-02-04 13:58:58 +0900

    index.css was not necessary anymore

--- components/layout.js
+++ components/layout.js
@@ -21,7 +21,6 @@ export default ({children}) => (
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.22/css/uikit.min.css" />
       <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.22/js/uikit.min.js"></script>
-      <link rel="stylesheet" href="index.css" />
     </Head>
     {children}
     <script src="bundle.js"></script>
