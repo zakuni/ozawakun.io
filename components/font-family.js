bb7e1bb33a9fd85a4fe4436bf25990328eea53b5
zakuni <kunio038@gmail.com>
2018-02-13 20:24:04 +0900

    use styled-jsx

--- components/font-family.js
+++ components/font-family.js
@@ -5,9 +5,29 @@ export const Letter = (props) => (
 );
 
 export const Space = (props) => (
-  <span className="space" style={props.style}>&nbsp;</span>
+  <span className="space" style={props.style}>
+    &nbsp;
+    <style jsx>{`
+      @media screen and (min-width: 520px) {
+        .space {
+          font-size: 0.1rem; }
+      @media screen and (max-width: 519px) {
+        .space {
+          font-size: 0.1rem; }
+    `}</style>
+  </span>
 );
 
 export const Period = (props) => (
-  <span className="period" style={props.style}>.</span>
+  <span className="period" style={props.style}>
+    .
+    <style jsx>{`
+      @media screen and (min-width: 520px) {
+        .period {
+          font-size: 0.9rem; } }
+      @media screen and (max-width: 519px) {
+        .period {
+          font-size: 1rem; } }
+    `}</style>
+  </span>
 );
