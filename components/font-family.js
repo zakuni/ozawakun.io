3d468ace4eca7d0931a48eedeb7f8bcadc1c4284
zakuni <kunio038@gmail.com>
2018-02-14 20:51:41 +0900

    fix media queries wasn't correctly defined

--- components/font-family.js
+++ components/font-family.js
@@ -8,12 +8,16 @@ export const Space = (props) => (
   <span className="space" style={props.style}>
     &nbsp;
     <style jsx>{`
-      @media screen and (min-width: 520px) {
-        .space {
-          font-size: 0.1rem; }
       @media screen and (max-width: 519px) {
         .space {
-          font-size: 0.1rem; }
+          font-size: 0.1rem;
+        }
+      }
+      @media screen and (min-width: 520px) {
+        .space {
+          font-size: 0.1rem;
+        }
+      }
     `}</style>
   </span>
 );
@@ -22,12 +26,16 @@ export const Period = (props) => (
   <span className="period" style={props.style}>
     .
     <style jsx>{`
-      @media screen and (min-width: 520px) {
-        .period {
-          font-size: 0.9rem; } }
       @media screen and (max-width: 519px) {
         .period {
-          font-size: 1rem; } }
+          font-size: 1rem;
+        }
+      }
+      @media screen and (min-width: 520px) {
+        .period {
+          font-size: 0.9rem;
+        }
+      }
     `}</style>
   </span>
 );
