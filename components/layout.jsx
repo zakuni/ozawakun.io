f30ae8a9563b41841efe8623bb069a33b8646340
zakuni <kunio038@gmail.com>
2018-10-21 00:44:47 +0900

    Revert "move some accounts to footer"
    This reverts commit 9a50717219b3a777168a8cecbea5e19309c15bb1.

--- components/layout.jsx
+++ components/layout.jsx
@@ -25,15 +25,5 @@ export default ({children}) => (
       <title>ozawakun.io</title>
     </Head>
     {children}
-    <footer className="uk-text-center uk-link-muted uk-text-meta uk-margin-medium-bottom">
-      <div className="uk-margin-small-bottom">
-        <a href="https://twitter.com/zakuni" target="_blank" rel="noopener" className="uk-icon-button uk-margin-small-right" uk-icon="twitter"></a>
-        <a href="https://www.facebook.com/zakuni" target="_blank" rel="noopener" className="uk-icon-button  uk-margin-small-right" uk-icon="facebook"></a>
-        <a href="https://www.linkedin.com/in/kunioozawa/" target="_blank" rel="noopener" className="uk-icon-button" uk-icon="linkedin"></a>
-      </div>
-      <div>
-        <a href="http://ozawakun.io">ozawakun.io</a>, since 2013.&nbsp;<a href="mailto:contactme&amp;#64;ozawakun.io">contactme&#64;ozawakun.io</a>
-      </div>
-    </footer>
   </div>
 )
