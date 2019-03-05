ef09776e0d21f0cb0aa6baff837e628a3301af5c
zakuni <kunio038@gmail.com>
2019-03-05 20:20:23 +0900

    use defaultpathmap

--- next.config.js
+++ next.config.js
@@ -2,13 +2,5 @@ const debug = process.env.NODE_ENV !== 'production'
 const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
-  pageExtensions: ['jsx'],
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      '/gallery': { page: '/gallery' },
-      '/now': { page: '/now' },
-      '/links': { page: '/links' }
-    }
-  }
+  pageExtensions: ['jsx']
 })
