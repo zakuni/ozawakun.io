e1eaeaefe761961e82bc4bae9ae006d616f1fb72
zakuni <kunio038@gmail.com>
2019-03-10 22:59:09 +0900

    fix test

--- test/TopBar.spec.js
+++ test/TopBar.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import TopBar from '@/components/TopBar.vue'
 
-describe('Logo', () => {
+describe('TopBar', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(TopBar)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
