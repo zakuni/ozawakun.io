f3e848edc7b1cbf8af828e567d771935940d1ca2
zakuni <kunio038@gmail.com>
2019-05-23 20:41:05 +0900

    Create privacy-policy.vue

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="uk-container uk-container-small uk-width-3-4@s uk-margin-remove-top">
+    <main>
+      <section id="about" class="uk-section uk-margin-remove-bottom">
+        <h2 class="uk-h3 uk-margin-medium-bottom">
+          Privacy Policy
+        </h2>
+        <div class="uk-margin-medium-bottom">
+          <p>
+            第三者に個人を特定できる情報を提供することはありません。
+          </p>
+          <p>
+            利便性の向上を目的として、個人を特定しない範囲にて、利用状況の収集を行うことがあります。
+          </p>
+          <p>
+            個人情報に関して適用される日本の法令を遵守するとともに、本ポリシーの内容を適宜見直しその改善に努めます。
+          </p>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
