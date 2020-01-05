df291458a90d83b16bb0286f6bfe919a91996f13
zakuni <kunio038@gmail.com>
2020-01-05 21:00:36 +0900

    update uikit

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -19,9 +19,9 @@ export default ({children}) => (
 
       <meta content="width=device-width, initial-scale=1" name="viewport" />
 
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.2.6/dist/css/uikit.min.css" />
+      <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.6/dist/js/uikit.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.6/dist/js/uikit-icons.min.js"></script>
       <link rel="stylesheet" href="/style.css" />
       <title>ozawakun.io</title>
     </Head>
