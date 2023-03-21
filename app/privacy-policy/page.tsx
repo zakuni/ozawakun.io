0515187338026966998f312afb07ec19c5e8c645
Kunio Ozawa <kunio038@gmail.com>
2023-03-21 22:59:54 +0900

    privacy-policy into app/

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import PrivacyPolicyPage from './privacy-policy-page';
+
+export const metadata = {
+  title: 'PRIVACY POLICY | ozawakun.io',
+};
+
+export default async function Page() {
+  return <PrivacyPolicyPage />;
+}
