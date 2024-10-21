304a7c50ae24f91497ec33e3b5e8ede2c2f1cc75
zakuni <kunio038@gmail.com>
2024-10-21 19:58:25 +0900

    update products page
    - add 揚げ物タイマー, zakuni components
    - disable link to 街合わせ, Boy2Man

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import PageTitle from '@/components/PageTitle'
 import zCalImg from '@/public/images/z-cal.png';
+import deepfryTimerIcon from '@/public/images/deepfry-timer_icon.png';
 import appStoreImg from '@/public/images/Download_on_the_App_Store_blk_.svg';
 
 export const metadata = {
@@ -14,10 +15,58 @@ export default function Page() {
         <section className='mt-12 sm:mt-24 mb-36'>
           <PageTitle>Products</PageTitle>
           <section id="products">
-            <div className="flex flex-col sm:flex-row place-items-center gap-y-8 sm:gap-y-0 sm:gap-x-8">
+            <article className="flex flex-col sm:flex-row place-items-center gap-y-8 sm:gap-y-0 sm:gap-x-8">
               <div className="uk-width-1-4@s uk-text-center justify-self-center text-center">
-                <a href="https://itunes.apple.com/us/app/zakuni電卓/id1465087955?l=ja&ls=1&mt=8" className='inline-block text-center'>
-                  <Image alt="zakuni電卓" className="shadow hover:shadow-sm rounded-3xl" src={zCalImg} width="170" />
+                <a href="https://apps.apple.com/jp/app/%E6%8F%9A%E3%81%92%E7%89%A9%E3%82%BF%E3%82%A4%E3%83%9E%E3%83%BC/id6736895253" target='_blank' className='inline-block text-center'>
+                  <Image alt="揚げ物タイマーのアイコン" className="shadow-lg hover:shadow-sm rounded-3xl" src={deepfryTimerIcon} width="170" />
+                </a>
+              </div>
+              <div className="uk-width-3-4@s">
+                <h5 className='mb-2'>
+                  揚げ物タイマー
+                </h5>
+                <p>
+                  当初はzakuniシリーズにしようかと思っていたのですが、よりニッチなところを攻めることにしました。
+                </p>
+                <p>小さいものをとにかくリリース、と考えた結果でもあります。</p>
+                <p>一方で、初めてSwiftUIを使って作ったアプリで、すなわち僕が初めてXcodeで開発〜リリースまでしたアプリということになります。</p>
+                <p>大学の頃に初めてiPhoneアプリを作ってみたいと思ってから、XcodeとInterface Builderに悩まされ、Objective-C、MacRuby、PhoneGap、Swift(はあんま触れてないけど)などを経て、時にはApp Storeのレビューで跳ね返されたまま公開せずに終わったアプリなんかもあったけども、今やほんの数日で作り始めからリリースまでできるようになりました。思えば遠くへ来たものです、僕も、Xcodeも。</p>
+                <div style={{paddingTop: '20px'}}>
+                  <a href="https://apps.apple.com/jp/app/%E6%8F%9A%E3%81%92%E7%89%A9%E3%82%BF%E3%82%A4%E3%83%9E%E3%83%BC/id6736895253" target='_blank'>
+                    <Image alt='link to app store' src={appStoreImg} />
+                  </a>
+                </div>
+              </div>
+            </article>
+            <hr className="my-12" />
+            <article className="flex flex-col sm:flex-row place-items-center gap-y-8 sm:gap-y-0 sm:gap-x-8">
+              <div className="uk-width-1-4@s uk-text-center justify-self-center text-center">
+                <a className="cursor-pointer hover:no-underline hover:text-gray-600" href="https://zakuni-components.pages.dev" target='_blank'>
+                  <div className="shadow hover:shadow-sm p-6 py-10 bg-white">
+                    zakuni components
+                  </div>
+                </a>
+              </div>
+              <div className="uk-width-3-4@s">
+                <h5 className='mb-2'>
+                  zakuni components
+                </h5>
+                <p>
+                  コンポーネント集を作りたいなとは以前から思っていたのです、デザインシステムとかね。
+                </p>
+                <p>
+                  そう思いながら早幾年。こういうものはきっと作り始めなければ始まらない、なので作り始めました。
+                </p>
+                <p>
+                  そして作り始めたなら公開してしまえばいい。今後少しずつ増やしていったりアップデートしていければいいですよね。
+                </p>
+              </div>
+            </article>
+            <hr className="my-12" />
+            <article className="flex flex-col sm:flex-row place-items-center gap-y-8 sm:gap-y-0 sm:gap-x-8">
+              <div className="uk-width-1-4@s uk-text-center justify-self-center text-center">
+                <a href="https://itunes.apple.com/us/app/zakuni電卓/id1465087955?l=ja&ls=1&mt=8" className='inline-block text-center' target='_blank'>
+                  <Image alt="zakuni電卓のアイコン" className="shadow hover:shadow-sm rounded-3xl" src={zCalImg} width="170" />
                 </a>
               </div>
               <div className="uk-width-3-4@s">
@@ -29,27 +78,25 @@ export default function Page() {
                 </p>
                 <p>そしたら出来たのです。電卓が。</p>
                 <div style={{paddingTop: '20px'}}>
-                  <a href="https://itunes.apple.com/us/app/zakuni電卓/id1465087955?l=ja&ls=1&mt=8">
+                  <a href="https://itunes.apple.com/us/app/zakuni電卓/id1465087955?l=ja&ls=1&mt=8" target='_blank'>
                     <Image alt='link to app store' src={appStoreImg} />
                   </a>
                 </div>
               </div>
-            </div>
+            </article>
           </section>
           <section id='legacy' className='mt-24'>
             <h3 className='text-xl tracking-wide mb-3'>Legacy</h3>
-            <p className='mb-12'>Something I made long long ago...</p>
+            <p className='mb-12'>Something I made long, long ago...</p>
             <div className="flex flex-col sm:flex-row place-items-center gap-y-8 sm:gap-y-0 sm:gap-x-8">
               <div className="uk-width-1-4@s text-center">
-                <a className="uk-link-reset" href="http://machiawase.herokuapp.com">
-                  <div className="shadow hover:shadow-sm p-6 py-10 bg-white text-nowrap">
-                    街合わせ
-                  </div>
-                </a>
+                <div className="p-6 py-10 bg-gray-100 text-nowrap">
+                  街合わせ
+                </div>
               </div>
               <div className="uk-width-3-4@s">
                 <h5 className='mb-2'>
-                  街合わせ
+                  街合わせ(公開停止)
                 </h5>
                 <p>
                   待ち合わせをするためのwebサービス。二つの場所の中間らへんの場所を教えてくれる。
@@ -62,15 +109,13 @@ export default function Page() {
             <hr className="my-12" />
             <div className="flex flex-col sm:flex-row place-items-center gap-y-8 sm:gap-y-0 sm:gap-x-8">
               <div className="uk-width-1-4@s text-center">
-                <a className="uk-link-reset" href="http://boy2man.herokuapp.com">
-                  <div className="shadow hover:shadow-sm p-6 py-10 bg-white">
-                    Boy2Man
-                  </div>
-                </a>
+                <div className="p-6 py-10 bg-gray-100">
+                  Boy2Man
+                </div>
               </div>
               <div className="uk-width-3-4@s">
                 <h5 className='mb-2'>
-                  Boy2Man
+                  Boy2Man(公開停止)
                 </h5>
                 <p>
                   ジャンケンマン。
